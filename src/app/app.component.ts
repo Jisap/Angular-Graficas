2e5fef69a843b338b584e9bbb46c5067eb21c280
Jorge Matos <jisapjisap@gmail.com>
2021-07-31 12:28:37 +0200

    Finalización app Angular-Gráficas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+//Oficialmente
+//npm install --save ng2-charts
+//npm install --savechart.js
+
+//Realmente
+//npm install chart.js@2.9.3 --save
+//npm install ng2 - charts@2.2.3 --save
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
